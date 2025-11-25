983d82ef928b06534b7d596bd0c4627774e4662e
unknown <hdas9599@gmail.com>
2025-11-26 00:33:45 +0530

    Remove generated files

--- script.d.ts
+++
@@ -1,5 +0,0 @@
-declare const boardElem: HTMLElement | null;
-declare let cols: number, rows: number;
-declare let prevNum: number;
-declare function initializeBoard(): void;
-//# sourceMappingURL=script.d.ts.map
